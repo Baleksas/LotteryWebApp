c561c5f22ea9920c42495cf4d5231a44deac2e82
Baleksas <alexasbag17@gmail.com>
2021-10-12 20:06:24 +0100

    feat: encryption/decryption/hash implementation for draws and passwords

--- lottery/views.py
+++ lottery/views.py
@@ -4,11 +4,15 @@ import logging
 from flask import Blueprint, render_template, request, flash
 
 from app import db
-from models import Draw
+from models import Draw, User
+
 
 # CONFIG
 lottery_blueprint = Blueprint('lottery', __name__, template_folder='templates')
 
+
+user = User.query.first()
+draw_key = user.draw_key
 
 # VIEWS
 # view lottery page
@@ -25,7 +29,7 @@ def add_draw():
     submitted_draw.strip()
 
     # create a new draw with the form data.
-    new_draw = Draw(user_id=1, draw=submitted_draw, win=False, round=0)  # TODO: update user_id [user_id=1 is a placeholder]
+    new_draw = Draw(user_id=1, draw=submitted_draw, win=False, round=0, draw_key=draw_key)  # TODO: update user_id [user_id=1 is a placeholder]
 
     # add the new draw to the database
     db.session.add(new_draw)
@@ -44,6 +48,9 @@ def view_draws():
 
     # if playable draws exist
     if len(playable_draws) != 0:
+        # decrypt every draw
+        for draw in playable_draws:
+            draw.view_draw(draw_key)
         # re-render lottery page with playable draws
         return render_template('lottery.html', playable_draws=playable_draws)
     else:
